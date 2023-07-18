0bc72a914e3a5ebe31ee9c7244f2627648a5826d
Alaister <77394050+FTP-droid@users.noreply.github.com>
2023-07-17 21:39:20 -0400

    Removed additional whitespace in parse_item

--- scrapywebscraper/spiders/crawling_spider.py
+++ scrapywebscraper/spiders/crawling_spider.py
@@ -13,7 +13,7 @@ class CrawlingSpider(CrawlSpider):
 
     def parse_item(self, response):
         yield {
-            "title": response.css( ".product_main h1::text").get(),
+            "title": response.css(".product_main h1::text").get(),
             "price": response.css(".price_color::text").get(),
             "availability": response.css(".availability::text")[1].get().strip(),
         }
